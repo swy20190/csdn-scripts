622c553bcbea0adde32765c5d6da4f7a196de6f0
swy20190 <K201410109@163.com>
2020-05-08 19:59:10 +0800

    blog commenter finished

---
+++ autoCommenter/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+public class Main {
+
+    public static void main(String[] args) throws AWTException {
+	// write your code here
+        Robot commenter = new Robot();
+        //鼠标准备
+        commenter.delay(3000);
+        //先来个100下
+        for(int i=0;i<100;i++){
+            commenter.mousePress(InputEvent.BUTTON1_MASK);
+            commenter.mouseRelease(InputEvent.BUTTON1_MASK);
+            commenter.mousePress(InputEvent.BUTTON1_MASK);
+            commenter.mouseRelease(InputEvent.BUTTON1_MASK);
+            commenter.delay(2000);
+            //开始输入
+            commenter.keyPress(KeyEvent.VK_CONTROL);
+            commenter.keyPress(KeyEvent.VK_V);
+            commenter.keyRelease(KeyEvent.VK_V);
+            commenter.keyRelease(KeyEvent.VK_CONTROL);
+            commenter.delay(200);
+            //commit comment
+            Point p = MouseInfo.getPointerInfo().getLocation();
+            commenter.mouseMove((int)p.getX(),(int)p.getY()+80);
+            commenter.delay(200);
+            commenter.mousePress(InputEvent.BUTTON1_MASK);
+            commenter.mouseRelease(InputEvent.BUTTON1_MASK);
+            p = MouseInfo.getPointerInfo().getLocation();
+            commenter.mouseMove((int)p.getX(),(int)p.getY()-80);
+            commenter.delay(1000);
+        }
+    }
+}
