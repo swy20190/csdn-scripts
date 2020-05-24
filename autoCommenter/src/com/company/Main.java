e4c6d6d6216a440296c8bea75878b52abe445260
swy20190 <K201410109@163.com>
2020-05-24 21:32:21 +0800

    adjust parameters

--- autoCommenter/src/com/company/Main.java
+++ autoCommenter/src/com/company/Main.java
@@ -10,28 +10,31 @@ public class Main {
         Robot commenter = new Robot();
         //鼠标准备
         commenter.delay(3000);
-        //先来个100下
-        for(int i=0;i<100;i++){
+        //先来个5下
+        for(int i=0;i<5;i++){
             commenter.mousePress(InputEvent.BUTTON1_MASK);
             commenter.mouseRelease(InputEvent.BUTTON1_MASK);
+            commenter.delay(200);
             commenter.mousePress(InputEvent.BUTTON1_MASK);
             commenter.mouseRelease(InputEvent.BUTTON1_MASK);
             commenter.delay(2000);
             //开始输入
             commenter.keyPress(KeyEvent.VK_CONTROL);
             commenter.keyPress(KeyEvent.VK_V);
+            commenter.delay(100);
             commenter.keyRelease(KeyEvent.VK_V);
             commenter.keyRelease(KeyEvent.VK_CONTROL);
             commenter.delay(200);
             //commit comment
             Point p = MouseInfo.getPointerInfo().getLocation();
-            commenter.mouseMove((int)p.getX(),(int)p.getY()+80);
+            commenter.mouseMove((int)p.getX(),(int)p.getY()+78);
             commenter.delay(200);
             commenter.mousePress(InputEvent.BUTTON1_MASK);
             commenter.mouseRelease(InputEvent.BUTTON1_MASK);
             p = MouseInfo.getPointerInfo().getLocation();
-            commenter.mouseMove((int)p.getX(),(int)p.getY()-80);
+            commenter.mouseMove((int)p.getX(),(int)p.getY()-78);
             commenter.delay(1000);
         }
     }
 }
+
